bdb4d1c93b014a3c83956edf9fb566f3fbb99575
debanaskar13 <debanaskar13@gmail.com>
2024-01-20 19:08:30 +0530

    Added Role Model , Services and Repositories

---
+++ src/main/java/com/example/first/microservice/service/impl/RoleServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.first.microservice.service.impl;
+
+import com.example.first.microservice.dto.RoleDto;
+import com.example.first.microservice.service.RoleService;
+
+import java.util.List;
+
+public class RoleServiceImpl implements RoleService {
+    @Override
+    public List<RoleDto> getAllRoles() {
+        return null;
+    }
+
+    @Override
+    public RoleDto getRoleById() {
+        return null;
+    }
+
+    @Override
+    public RoleDto getRoleByName() {
+        return null;
+    }
+
+    @Override
+    public String createRole() {
+        return null;
+    }
+
+    @Override
+    public String updateRole() {
+        return null;
+    }
+
+    @Override
+    public void deleteRole() {
+
+    }
+}
