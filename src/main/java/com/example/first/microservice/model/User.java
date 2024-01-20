7a56f06e20ed0215c13e6246fe03246564e81d6c
debanaskar13 <debanaskar13@gmail.com>
2024-01-20 17:21:29 +0530

    Created User Model

--- src/main/java/com/example/first/microservice/model/User.java
+++ src/main/java/com/example/first/microservice/model/User.java
@@ -1,6 +1,9 @@
 package com.example.first.microservice.model;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.Instant;
 
@@ -10,15 +13,47 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(name = "username",nullable = false,unique = true)
     private String username;
+
+    @Column(name = "password",nullable = false)
     private String password;
+
+    @Column(name = "email",nullable = false,unique = true)
     private String email;
+
+    @Column(name = "first_name",nullable = false)
     private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "last_name",nullable = false)
     private String lastName;
+
+    @Column(name = "mobile_number",unique = true)
+    private String mobileNumber;
+
+    @Column(name = "intro")
+    private String intro;
+
+    @Column(name = "profile")
+    private String profile;
+
+    @Column(name = "registered_at",nullable = false)
+    @CreatedDate
     private Instant registeredAt;
+
+    @Column(name = "last_login_at",nullable = false)
+    @LastModifiedDate
     private Instant lastLoginAt;
 
 }
